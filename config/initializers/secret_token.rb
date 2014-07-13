e62d3d8c09ab8ae9607fcc330b451da607933651
Lixiang Ding <zhimadlx@gmail.com>
2014-07-13 19:38:46 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SntonCom::Application.config.secret_key_base = 'd1d67df9ecd985d2c714f366468f03d6bb3ed37027d504fa8af29d9e8ff26c0702234cd3cc7ecbaed61848aa757d4241f31be9a205041f774b710245e71e3ed5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SntonCom::Application.config.session_store :cookie_store, key: '_snton_com_session'
